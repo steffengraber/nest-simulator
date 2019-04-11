e6098989cc76a8a66ac1f5d3aafaaab8aa2cd97a
Steffen Graber <s.graber@fz-juelich.de>
2019-04-11 15:21:33 +0200

    test without  testing for rtd

--- doc/conf.py
+++ doc/conf.py
@@ -131,11 +131,11 @@ breathe_default_project = "EXTRACT_MODELS"
 #    'backreferences_dir': False
 # }
 
-read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+# read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
-if read_the_docs_build:
+# if read_the_docs_build:
 
-    subprocess.call('doxygen', shell=True)
+subprocess.call('doxygen', shell=True)
 
 
 sphinx_gallery_conf = {
