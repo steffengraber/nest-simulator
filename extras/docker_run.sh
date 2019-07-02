ef218dc3c21beaee4be59d2cf1a4a7a66c4157ca
Steffen Graber <s.graber@fz-juelich.de>
2019-07-02 11:57:29 +0200

    set test to 1

--- extras/docker_run.sh
+++ extras/docker_run.sh
@@ -4,4 +4,4 @@ docker pull steffengraber/travis-nest:latest
 docker run -it --rm -d --user nest --name travis-test-nest -v $(pwd):/home/nest/data steffengraber/travis-nest:latest
 
 docker ps -a
-docker container exec -it --env "TEST=0" --user nest travis-test-nest sh /home/nest/data/extras/travis_tests.sh
+docker container exec -it --env "TEST=1" --user nest travis-test-nest sh /home/nest/data/extras/travis_tests.sh
