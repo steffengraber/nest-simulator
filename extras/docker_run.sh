e14a717cc9f8cd62aea99f676389beb0615a3946
Steffen Graber <s.graber@fz-juelich.de>
2019-07-02 11:40:08 +0200

    using []

--- extras/docker_run.sh
+++ extras/docker_run.sh
@@ -3,6 +3,5 @@
 docker pull steffengraber/travis-nest:latest
 docker run -it --rm -d --user nest --name travis-test-nest -v $(pwd):/home/nest/data steffengraber/travis-nest:latest
 
-
 docker ps -a
 docker container exec -it --env "TEST=0" --user nest travis-test-nest sh /home/nest/data/extras/travis_tests.sh
