ecc0882022091ae2476ac169a10d8f62c4921e8b
Steffen Graber <s.graber@fz-juelich.de>
2022-05-02 15:06:45 +0200

    Fix for ppa v3.3

--- sli/slistartup.cc
+++ sli/slistartup.cc
@@ -135,10 +135,10 @@ SLIStartup::GetenvFunction::execute( SLIInterpreter* i ) const
 
 
 SLIStartup::SLIStartup( int argc, char** argv )
-  : sliprefix( NEST_INSTALL_PREFIX )
-  , slilibdir( sliprefix + "/" + NEST_INSTALL_DATADIR )
-  , slidocdir( sliprefix + "/" + NEST_INSTALL_DOCDIR )
-  , startupfile( slilibdir + "/sli/sli-init.sli" )
+  : sliprefix( "/usr/" )
+  , slilibdir( "/usr/share/nest/")
+  , slidocdir( "/usr/share/doc/nest/"  )
+  , startupfile( slilibdir + "/usr/share/nest/sli/sli-init.sli" )
   , verbosity_( SLIInterpreter::M_INFO ) // default verbosity level
   , debug_( false )
   , argv_name( "argv" )
