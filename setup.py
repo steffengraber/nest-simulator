e5edff140f752589f3e6559810a68353c1f95d6b
Steffen Graber <s.graber@fz-juelich.de>
2022-09-26 13:36:38 +0200

    Added deps

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 # from setuptools import setup
-from skbuild import setup  # This line replaces 'from setuptools import setup'
+from skbuild import setup
+import re
+from pathlib import Path
+
+for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
+    i.write_text(re.sub("^//.*$\n^[^#].*pip-build-env.*$", "", i.read_text(), flags=re.M))
 
 setup(
-    name='nest-wheel-test',
-    version='0.1.2',
+    name='nest_wheel',
+    version='0.1.2set',
     author="steffengraber",
     author_email="s.graber@fz-juelich.de",
     description="A test for nest",
     url="https://github.com/steffengraber/nest-simulator.git",
     download_url="https://github.com/steffengraber/nest-simulator/archive/refs/heads/nest-wheel.zip",
     packages=['nest'],
-    python_requires=">=3.8",
+    python_requires=">=3.8, <4",
+    install_requires=[
+            'libtool',
+            'pytest',
+            'pytest-timeout',
+            'pytest-xdist',
+            'numpy',
+            'pandas',
+            'scipy',
+            'mpi4py',
+            'terminaltables',
+            'pycodestyle'
+      ],
     cmake_install_dir='.'
     )
