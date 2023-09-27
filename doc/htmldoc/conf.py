f84a19da522b9bf69469cca3691e96559c2d96c3
Steffen Graber <s.graber@fz-juelich.de>
2023-09-27 06:26:14 +0000

    Run examples with ^b

--- doc/htmldoc/conf.py
+++ doc/htmldoc/conf.py
@@ -79,8 +79,7 @@ if ci_full_doc == "ON":
         "examples_dirs": "../../pynest/examples",
         # path where to save gallery generated examples
         "gallery_dirs": "auto_examples",
-        "filename_pattern": "/",
-        "ignore_pattern": r"__init__\.py",
+        "filename_pattern": "/b",
         "ignore_pattern": r"*\/sudoku\/*",
         "plot_gallery": "True",
         "download_all_examples": False,
