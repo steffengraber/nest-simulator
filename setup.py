80ff6fec8b2b32ee2005ec80fe4dd42289d6475b
Steffen Graber <s.graber@fz-juelich.de>
2024-03-19 10:16:34 +0000

    Add pynest and testing requirements

--- setup.py
+++ setup.py
@@ -1,32 +1,65 @@
 # from setuptools import setup
-from skbuild import setup
 import re
 from pathlib import Path
+
+from skbuild import setup
 
 for i in (Path(__file__).resolve().parent / "_skbuild").rglob("CMakeCache.txt"):
     i.write_text(re.sub("^//.*$\n^[^#].*pip-build-env.*$", "", i.read_text(), flags=re.M))
 
 setup(
-    name='nest_wheel',
-    version='0.1.2set',
+    name="nest_wheel",
+    version="0.1.4",
     author="steffengraber",
     author_email="s.graber@fz-juelich.de",
     description="A test for nest",
     url="https://github.com/steffengraber/nest-simulator.git",
     download_url="https://github.com/steffengraber/nest-simulator/archive/refs/heads/nest-wheel.zip",
-    packages=['nest'],
+    packages=["nest"],
     python_requires=">=3.8, <4",
     install_requires=[
-            'libtool',
-            'pytest',
-            'pytest-timeout',
-            'pytest-xdist',
-            'numpy',
-            'pandas',
-            'scipy',
-            'mpi4py',
-            'terminaltables',
-            'pycodestyle'
-      ],
-    cmake_install_dir='.'
-    )
+        "boost",
+        "numpy",
+        "pandas",
+        "scipy",
+        "matplotlib",
+        "mpi4py",
+        "h5py",
+        "black",
+        "pre-commit",
+        "isort",
+        "notebook",
+        "jupyterlab",
+        "csa",
+        "pydot",
+        # TESTING
+        "pytest",
+        "pytest-timeout",
+        "pytest-xdist",
+        "pytest-pylint",
+        "pytest-mypy",
+        "pytest-cov",
+        "data-science-types",
+        "terminaltables",
+        "pycodestyle",
+        "pydocstyle",
+        "rstcheck",
+        "mypy >= 0.8",
+        "junitparser >= 2",
+        "clang-format == 17.0.4",
+        "attrs",
+    ],
+    cmake_install_dir=".",
+    cmake_args=[
+        "-Dwith-python=ON",
+        "-Dwith-boost=ON",
+        "-Dwith-gsl=ON",
+        "-Dwith-readline=ON",
+        "-Dwith-ltdl=ON",
+        "-Dwith-openmp=ON",
+    ],
+)
+# - Could NOT find Readline (missing: READLINE_LIBRARY NCURSES_LIBRARY READLINE_INCLUDE_DIR)
+# -- Could NOT find PkgConfig (missing: PKG_CONFIG_EXECUTABLE)
+# -- Could NOT find GSL (missing: GSL_INCLUDE_DIR GSL_LIBRARY GSL_CBLAS_LIBRARY) (found version "")
+# -- Could NOT find Boost (missing: Boost_INCLUDE_DIR) (Required is at least version "1.69.0")
