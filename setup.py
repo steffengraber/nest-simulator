556c64e81d483e6f7bbb362df8aa5d3381e8542b
Steffen Graber <s.graber@fz-juelich.de>
2024-10-29 12:10:11 +0100

    fix naming

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="my_package",
+    name="nest-simulator",
     # version='0.1.0',
     # ... other setup.py details ...
     # Install `modify_activate.py` as a script
-    install_scripts=["scripts/modify_activate.py"],
+    install_scripts=["build_support/modify_activate.py"],
     # Define an entry point to automatically run `modify_activate` after installation
     entry_points={
         "console_scripts": [
